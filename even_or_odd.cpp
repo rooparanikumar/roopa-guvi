d29dd740b0072237c4bf4ef61200b9922721d13b
rooparanikumar <rooparanikumar@gmail.com>
2016-06-23 09:55:24 +0530

    Create even_or_odd.cpp

---
+++ even_or_odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int number;
+	cin>>number;
+	if(number%2==0)
+	    cout<<"Number is even";
+	    else
+	        cout<<"Number is odd";
+	return 0;
+}
